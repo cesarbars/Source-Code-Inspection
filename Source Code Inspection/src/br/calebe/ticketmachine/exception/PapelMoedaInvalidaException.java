44df7fc03caef1ac5a22483b9272a7d3d3e72c6b
41242661 <41242661@092943-P31S401.macklabs.br>
2015-09-23 11:49:49 -0300

    Corrige exeption para exibir mensagem

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -3,7 +3,11 @@ package br.calebe.ticketmachine.exception;
 /**
  *
  * @author Calebe de Paula Bianchini
+ *
  */
+
 public class PapelMoedaInvalidaException extends Exception {
-    
+    public PapelMoedaInvalidaException(String message) {
+        message = "Falha ao validar papel moeda";
+    }
 }
